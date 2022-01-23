a09e4d9b708ac5cad3efddbb8014a1c4eea58fcc
Akinator1 <kallassisimon@gmail.com>
2022-01-23 11:32:43 -0500

    Created assignment 1 CPP

---
+++ Assignment 1/Hotel Reservation System/Hotel Reservation System/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hotel Reservation System
+//
+//  Created by Simon El Kallassi on 2022-01-23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
